397595bcadc194238dccf15ce33a14e5e1416e59
WisdomWolf <wisdomwolf@gmail.com>
2018-02-20 10:29:40 -0600

    beginning migration of custom spotipy classes to client

--- spotipy/client.py
+++ spotipy/client.py
@@ -29,6 +29,143 @@ class SpotifyException(Exception):
             self.http_status, self.code, self.msg)
 
 
+class _SpotipyBase(object):
+
+    def __init__(self, href=None, type=None, uri=None):
+        self.href = href
+        self.type = type
+        self.uri = uri
+
+    def __repr__(self):
+        repr_list = []
+        for k, v in self.__dict__.items():
+            if v:
+                if isinstance(v, date):
+                    v = '{:%m/%d/%Y}'.format(v)
+                k = k.replace('_', ' ')
+                repr_list.append('{}={}'.format(k, v))
+        return '{}({})'.format(self.__class__.__name__, ', '.join(sorted(repr_list, key=self._sort)))
+
+    @staticmethod
+    def _sort(key):
+        '''Used to ensure certain attributes are listed first'''
+        key = key.split(':')[0].lower()
+        sort_keys = ['id', 'name'] # Should probably be a class variable so that it can be easily overridden
+        try:
+            return sort_keys.index(key)
+        except ValueError:
+            return float('inf')
+
+
+
+class SpotipyArtist(_SpotipyBase):
+
+    def __init__(self, id, name, external_urls=None, href=None, type=None, uri=None, *args, **kwargs):
+        self.id = id
+        self.name = name
+        self.external_urls=external_urls
+        self.href = href
+        self.type = type
+        self.uri = uri
+        self._args = args
+        self._kwargs = kwargs
+
+
+class SpotipyAlbum(_SpotipyBase):
+
+    def __init__(self, id, name, album_type=None, artists=None, available_markets=None, external_urls=None, href=None, images=None, type=None, uri=None, *args, **kwargs):
+        self.id = id
+        self.name = name
+        self.external_urls = external_urls or []
+        self._artists = [SpotipyArtist(**artist) if not isinstance(artist, SpotipyArtist) else artist for artist in artists]
+        self.artist = self._artists[0].name
+        self.available_markets = available_markets or []
+        self.href = href
+        self.images = images or []
+        self.type = type
+        self.uri = uri
+        self._args = args
+        self._kwargs = kwargs
+
+
+class SpotipyTrack(_SpotipyBase):
+
+    def __init__(self, id, name, album, artists, available_markets=None, disc_number=None, 
+    duration_ms=0, explicit=False, external_ids=None, external_urls=None, href=None, 
+    popularity=None, preview_url=None, track_number=None, type=None, uri=None, *args, **kwargs):
+        #print('__init__ received')
+       # for param, value in locals().items():
+          #  print('{}: {}\n'.format(param, value))
+        self.id = id
+        self.name = name
+        if isinstance(album, SpotipyAlbum):
+            self.album = album
+        else:
+            self.album = SpotipyAlbum(**album)
+        self._artists = [SpotipyArtist(**artist) if not isinstance(artist, SpotipyArtist) else artist for artist in artists]
+        self.artist = self._artists[0].name
+        self.available_markets = available_markets or []
+        self.disc_number = disc_number
+        self.duration_ms = duration_ms
+        self.duration = self.duration_ms // 1000
+        self.explicit = explicit
+        self.external_ids = external_ids
+        self.external_urls = external_urls
+        self.href = href
+        self.popularity = popularity
+        self.preview_url = preview_url
+        self.track_number = track_number
+        self.type = type
+        self.uri = uri
+        self._args = args
+        self._kwargs = kwargs
+
+
+class SpotipyPlayback(_SpotipyBase):
+
+    def __init__(self, item, timestamp=None, progress_ms=None, is_playing=False, context=None, *args, **kwargs):
+       # print('__init__ received')
+     #   for param, value in locals().items():
+         #   print('{}: {}\n'.format(param, value))
+        self.track = item if isinstance(item, SpotipyTrack) else SpotipyTrack(**item)
+        self.timestamp = timestamp or time.time()
+        self.epoch_timestamp = self.timestamp // 1000
+        self.progress_ms = progress_ms or 0
+        self.is_playing = is_playing
+        self.context = context
+        self._args = args
+        self._kwargs = kwargs
+
+    @property
+    def progress(self):
+        return round(self.progress_ms / self.track.duration_ms * 100, 0)
+
+
+class SpotipyPlaylist(_SpotipyBase):
+
+    def __init__(self, id, name, owner, collaborative=False, description=None, external_urls=None, 
+    followers=None, href=None, images=None, public=True, snapshot_id=None, tracks=None, type=None, uri=None, sp=None, precache=False):
+        self.id = id
+        self.name = name
+        self.owner = owner
+        self.collaborative = collaborative
+        self.description = description
+        self.external_urls = external_urls
+        self.followers = followers
+        self.href = href
+        self.images = images
+        self.public = public
+        self.snapshot_id = snapshot_id
+        self._tracks = tracks
+        self.type = type
+        self.uri = uri
+        self.tracks = [SpotipyTrack(**track.get('track')) for track in self._tracks.get('items')]
+        if precache:
+            while self._tracks['next']:
+                self._tracks = sp.next(self._tracks)
+                self.tracks.extend([SpotipyTrack(**track.get('track')) for track in self._tracks.get('items')])
+
+
 class Spotify(object):
     """
         Example usage::
